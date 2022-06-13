c81008cecee080334baa1089dd48a4df9fb459d8
Lukasz Kot <lukasz.kot@hotmail.com>
2022-06-13 13:29:25 +0200

    TagsInput component added to the project.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import './App.css';
+import TagsInput from './components/TagsInput';
 
 function App() {
-	return <div className='App'></div>;
+	return (
+		<div className='App'>
+			<h2>Enter Your Skills...</h2>
+			<TagsInput />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/TagsInput.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './TagsInput.css';
+
+const TagsInput = () => {
+    return (
+		<>
+			<div className='tags-input-container'>
+                TagsInput Component
+			</div>
+		</>
+	);
+};
+
+export default TagsInput;
